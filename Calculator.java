a7c2eb27e97090337a0a5c9f03d26e2b0f38f3e1
사라 <prinsaraouo@gmail.com>
2026-03-20 11:06:02 +0900

    정수 입력칸 ㄹㅇㄹㅇ 처음

---
+++ Calculator.java
@@ -0,0 +1,18 @@
+package ohyeah.calculator;
+
+import java.util.Scanner;
+
+public class Calculator {
+    static void main(String[] args) {
+
+        // sc 변수, System.in = 키보드 입력 ㅇㅋ
+       Scanner sc = new Scanner(System.in);
+
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int apple = sc.nextInt();
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int banana = sc.nextInt();
+
+
+    }
+}
