c239f941ee32850e5271bf398a90b636e3a384a2
사라 <prinsaraouo@gmail.com>
2026-03-20 14:47:26 +0900

    exit 구간 1

--- Calculator.java
+++ Calculator.java
@@ -4,41 +4,49 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        // sc 변수, System.in = 키보드 입력 ㅇㅋ
-       Scanner sc = new Scanner(System.in);
-       int result = 0;
-       int e = 0;
+        int result = 0;
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        int apple = sc.nextInt(); //첫 번째 정수 입력 받는 칸
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int banana = sc.nextInt(); //두 번째 정수 입력 받는 칸
+        for (; ; ) {
 
+            System.out.println("첫 번재 숫자를 입력하세요: ");
+            int apple = sc.nextInt();
 
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        char melon = sc.next() .charAt(0); //문자열로 받고 맨 앞글자 꺼내기, Scanner는 글자 하나를 못 꺼낸다.
+            System.out.println("사칙연산 기호를 입력하세요: ");
+            char melon = sc.next().charAt(0);
 
-        // hi
-        // h위에까진 맞았죠?^^ 개큰성장~! 님
-        // 근데
-    if ( melon == '+') {
-       result = apple + banana;
-    }
-    if ( melon == '-') {
-       result = apple - banana;
-    }
-    if ( melon == '*') {
-        result = apple * banana;
-    }
-    if ( melon == '/') {
-        if ( banana == 0 ) {
-            System.out.println(" 0은안돼ㅇㄴㄹㅇㄴㅁㄹ ");
+            System.out.println("두 번째 숫자를 입력하세요: ");
+            int banana = sc.nextInt();
+
+            if (banana == 0 && melon == '/') {
+                System.out.println(" 나눗셈일 때 두 번째 숫자가 0이면 안돼용");
+                continue;
+            }
+
+            if ( melon == '+'){
+                result = apple + banana;
+            }
+
+            if ( melon == '-'){
+                result = apple - banana;
+            }
+
+            if ( melon == '*'){
+                result = apple * banana;
+            }
+
+            if ( melon == '/'){
+                result = apple / banana;
+            }
+
+            System.out.println("결과: " + result );
+
+            System.out.println("더 계산하시겟습니까? (exit 입력 시 종료 ");
+            String exit = sc.nextLine();
+
         }
-        else result = apple / banana;
 
-    }
-        System.out.println("결과: " + result);
 
     }
 }
