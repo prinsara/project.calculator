1ae21a4b3b82d294306605401499503618d3c98a
사라 <prinsaraouo@gmail.com>
2026-03-20 15:19:28 +0900

    사칙연산 기호란에 사칙연만 기호만 넣을 수 있음으로 수정, exit 입력시 종료 완료

--- Calculator.java
+++ Calculator.java
@@ -15,6 +15,11 @@ public class Calculator {
 
             System.out.println("사칙연산 기호를 입력하세요: ");
             char melon = sc.next().charAt(0);
+            if (!(melon == '+' || melon == '-' || melon == '*' || melon == '/')) {
+                System.out.println("사칙연산 기호만 입력 처음으로 돌아가기 고고");
+                continue;
+            }
+
 
             System.out.println("두 번째 숫자를 입력하세요: ");
             int banana = sc.nextInt();
@@ -42,11 +47,11 @@ public class Calculator {
 
             System.out.println("결과: " + result );
 
-            System.out.println("더 계산하시겟습니까? (exit 입력 시 종료 ");
-            String exit = sc.nextLine();
-
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료) ");
+            String exit = sc.next();
+            if (exit.equals("exit")) { //.equals 활용 : 문자열 내용이 같은지
+                break;
+            }
         }
-
-
     }
 }
