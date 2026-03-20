dc76badaaa9e6a85d21b4d43cdf13499c7860af9
사라 <prinsaraouo@gmail.com>
2026-03-20 11:24:22 +0900

    사칙연산 기호 입력칸 추가 + 메인 시작점 수정

--- Calculator.java
+++ Calculator.java
@@ -3,15 +3,21 @@ package ohyeah.calculator;
 import java.util.Scanner;
 
 public class Calculator {
-    static void main(String[] args) {
+    public static void main(String[] args) {
 
         // sc 변수, System.in = 키보드 입력 ㅇㅋ
        Scanner sc = new Scanner(System.in);
 
         System.out.print("첫 번째 숫자를 입력하세요: ");
-        int apple = sc.nextInt();
+        int apple = sc.nextInt(); //첫 번째 정수 입력 받는 칸
         System.out.print("두 번째 숫자를 입력하세요: ");
-        int banana = sc.nextInt();
+        int banana = sc.nextInt(); //두 번째 정수 입력 받는 칸
+
+
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char melon = sc.next() .charAt(0); //문자열로 받고 맨 앞글자 꺼내기, Scanner는 글자 하나를 못 꺼낸다.
+
+
 
 
     }
